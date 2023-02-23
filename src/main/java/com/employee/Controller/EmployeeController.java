a0726ab5cccb33cd6a078ac1ac2f6046804d1057
Josh Lovin <jdlovin7185@gmail.com>
2023-02-22 21:22:45 -0500

    fixed endpoints

--- src/main/java/com/employee/Controller/EmployeeController.java
+++ src/main/java/com/employee/Controller/EmployeeController.java
@@ -5,22 +5,36 @@ import com.employee.Service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("api/employee")
 @Controller
 public class EmployeeController {
 
     @Autowired
     EmployeeService service;
 
-    @PostMapping("employee/add")
+    @PostMapping("/add")
     public Employee addEmployee(@RequestBody Employee employee){
+
         return service.addEmployee(employee);
     }
 
-    @PostMapping("employee/update")
+    @PostMapping("/update")
     public Employee updateEmployee(@RequestBody Employee employee, Long id){
         return service.updateEmployee(id, employee);
     }
+
+    @DeleteMapping("/delete")
+    public void deleteEmployee(@RequestBody Employee employee){
+        service.deleteEmployee(employee);
+    }
+
+    @GetMapping("/")
+    public String randomMessage(){
+        return "Hi lil bitch";
+    }
 }
+
+
